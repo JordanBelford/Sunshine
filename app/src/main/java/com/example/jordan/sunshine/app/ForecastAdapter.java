a4fdd67485c4d4fd3b8d540f87939e8fb539d59c
jordain <spam3alot@gmail.com>
2015-04-09 00:15:43 -0700

    Make today's weather more prominent.
    Today's weather uses a different layout than forecasts for future days.

--- app/src/main/java/com/example/jordan/sunshine/app/ForecastAdapter.java
+++ app/src/main/java/com/example/jordan/sunshine/app/ForecastAdapter.java
@@ -16,6 +16,11 @@ import com.example.jordan.sunshine.R;
  * from a {@link android.database.Cursor} to a {@link android.widget.ListView}.
  */
 public class ForecastAdapter extends CursorAdapter {
+
+    private static final int VIEW_TYPE_TODAY = 0;
+    private static final int VIEW_TYPE_FUTURE_DAY = 1;
+    private static final int VIEW_TYPE_COUNT = 2;
+
     public ForecastAdapter(Context context, Cursor c, int flags) {
         super(context, c, flags);
     }
@@ -43,13 +48,30 @@ public class ForecastAdapter extends CursorAdapter {
                 " - " + highAndLow;
     }
 
-    /*
-        Remember that these views are reused as needed.
-     */
+    @Override
+    public int getItemViewType(int position) {
+        return position == 0 ? VIEW_TYPE_TODAY : VIEW_TYPE_FUTURE_DAY;
+    }
+
+    @Override
+    public int getViewTypeCount() {
+        return VIEW_TYPE_COUNT;
+    }
+
     @Override
     public View newView(Context context, Cursor cursor, ViewGroup parent) {
+        // Choose the layout type
+        int viewType = getItemViewType(cursor.getPosition());
+        int layoutId = -1;
 
-        return LayoutInflater.from(context).inflate(R.layout.list_item_forecast, parent, false);
+        // Determine layoutId from viewType
+        if(viewType == VIEW_TYPE_TODAY) {
+            layoutId = R.layout.list_item_forecast_today;
+        }
+        else if(viewType == VIEW_TYPE_FUTURE_DAY) {
+            layoutId = R.layout.list_item_forecast;
+        }
+        return LayoutInflater.from(context).inflate(layoutId, parent, false);
     }
 
     /*
