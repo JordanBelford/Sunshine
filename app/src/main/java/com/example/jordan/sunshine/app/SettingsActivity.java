88ed234449e87d195be68e408c6ed9707a6628fd
Jordan Belford <jordan.g.belford@gmail.com>
2015-05-11 22:37:24 -0700

    Bugfix for settings activity

--- app/src/main/java/com/example/jordan/sunshine/app/SettingsActivity.java
+++ app/src/main/java/com/example/jordan/sunshine/app/SettingsActivity.java
@@ -1,7 +1,9 @@
 package com.example.jordan.sunshine.app;
 
+import android.annotation.TargetApi;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.preference.ListPreference;
 import android.preference.Preference;
@@ -106,6 +108,12 @@ public class SettingsActivity extends PreferenceActivity
 
 
         return true;
+    }
+
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+    @Override
+    public Intent getParentActivityIntent() {
+        return super.getParentActivityIntent().addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
     }
 
 }
